7b3e2d282f54e3e9d1141916f58fe9c3c330e07f
Nick McKillip <nmckillip1@gmail.com>
2017-10-21 15:03:44 -0500

    second commit

--- util.py
+++ util.py
@@ -3,226 +3,61 @@ import pandas as pd
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LogisticRegression
+
+def init_weight_and_bias(M1, M2):
+    W = np.random.randn(M1, M2) / np.sqrt(M1 + M2)
+    b = np.zeros(M2)
+    return W.astype(np.float32), b.astype(np.float32)
+
+def init_filter(shape, poolsz):
+    w = np.random.randn(*shape) /np.sqrt(np.prod(shape[1:]) + shape[0]*np.prod(shape[2:] / np.prod(poolsz)))
+    return w.astype(np.float32)
 
 def error_rate(p, t):
     return np.mean(p != t)
 
 def y2indicator(y):
     N = len(y)
-    ind = np.zeros((N, 10))
+    K = len(set(y))
+    ind = np.zeros((N, K))
     for i in range(N):
         ind[i, int(y[i])] = 1
     return ind
 
 
-def get_transformed_data():
-	print("Reading and transforming data")
-	df = pd.read_csv('../large_files/train.csv')
-	data = df.as_matrix().astype(np.float32)
-	np.random.shuffle(data)
 
-	X = data[:, 1:]
-	mu = X.mean(axis=0)
-	X = X - mu
-	pca = PCA()
-	Z = pca.fit_transform(X)
-	Y = data [:,0]
-	return Z, Y, pca, mu
-
-def get_normalized_data():
-	print("Reading and transforming data")
-	df = pd.read_csv('../large_files/train.csv')
-	data = df.as_matrix().astype(np.float32)
-	np.random.shuffle(data)
-	X = data[:, 1:]
-	mu = X.mean(axis=0)
-	std = X.std(axis=0)
-	np.place(std, std ==0 , 1)
-	X = (X - mu) / std # normalize the data
-	Y = data[:, 0]
-	return X, Y
-
-
-def plot_cumulative_variance(pca):
-    P = []
-    for p in pca.explained_variance_ratio_:
-        if len(P) == 0:
-            P.append(p)
+def getData(balance_ones = True):
+    #images are 48x48 = 2304 size vectors
+    # N = 35887
+    Y = []
+    X = []
+    first = True
+    for line in open('fer2013/fer2013.csv'):
+        if first:
+            first = False
         else:
-            P.append(p + P[-1])
-    plt.plot(P)
-    plt.show()
-    return P
+            row = line.split(',')
+            Y.append(int(row[0]))
+            X.append([int(p) for p in row[1].split()])
+
+    X, Y = np.array(X) / 255.0, np.array(Y)
+
+    if balance_ones:
+        # repeat class 1 to balance classes
+        X0, Y0 = X[Y!=1, :], Y[Y!= 1]
+        X1 = X[Y==1, :]
+        X1 = np.repeat(X1, 9, axis= 0)
+        X = np.vstack([X0, X1])
+        Y = np.concatenate((Y0, [1]*len(X1)))
+
+    return X, Y
+
+def getImageData():
+    X, Y = getData()
+    N, D = X.shape
+    d = int(np.sqrt(D))
+    X = X.reshape(N, 1, d, d)
+    return X, Y
 
 
-
-def forward(X, W, b):
-	a  = X.dot(W) + b
-	expa = np.exp(a)
-	y = expa / expa.sum(axis=1, keepdims = True)
-	return y 
-
-def predict(p_y):
-	return np.argmax(p_y, axis=1)
-
-
-def cost(p_y, t):
-	tot = t * np.log(p_y)
-	return -tot.sum()
-
-def gradW(t, y, X):
-	return X.T.dot(t-y)
-
-def gradb(t,y):
-	return (t-y).sum(axis=0)
-
-def plot_cumulative_variance(pca):
-    P = []
-    for p in pca.explained_variance_ratio_:
-        if len(P) == 0:
-            P.append(p)
-        else:
-            P.append(p + P[-1])
-    plt.plot(P)
-    plt.show()
-    return P
-
-
-def benchmark_full():
-    X, Y = get_normalized_data()
-
-    print("Performing logistic regression...")
-    # lr = LogisticRegression(solver='lbfgs')
-
-    # # test on the last 1000 points
-    # lr.fit(X[:-1000, :200], Y[:-1000]) # use only first 200 dimensions
-    # print lr.score(X[-1000:, :200], Y[-1000:])
-    # print "X:", X
-
-    # normalize X first
-    # mu = X.mean(axis=0)
-    # std = X.std(axis=0)
-    # X = (X - mu) / std
-
-    Xtrain = X[:-1000,]
-    Ytrain = Y[:-1000]
-    Xtest  = X[-1000:,]
-    Ytest  = Y[-1000:]
-
-    # convert Ytrain and Ytest to (N x K) matrices of indicator variables
-    N, D = Xtrain.shape
-    Ytrain_ind = y2indicator(Ytrain)
-    Ytest_ind = y2indicator(Ytest)
-
-    W = np.random.randn(D, 10) / 28
-    b = np.zeros(10)
-    LL = []
-    LLtest = []
-    CRtest = []
-
-    # reg = 1
-    # learning rate 0.0001 is too high, 0.00005 is also too high
-    # 0.00003 / 2000 iterations => 0.363 error, -7630 cost
-    # 0.00004 / 1000 iterations => 0.295 error, -7902 cost
-    # 0.00004 / 2000 iterations => 0.321 error, -7528 cost
-
-    # reg = 0.1, still around 0.31 error
-    # reg = 0.01, still around 0.31 error
-    lr = 0.00004
-    reg = 0.01
-    for i in range(500):
-        p_y = forward(Xtrain, W, b)
-        # print "p_y:", p_y
-        ll = cost(p_y, Ytrain_ind)
-        LL.append(ll)
-
-        p_y_test = forward(Xtest, W, b)
-        lltest = cost(p_y_test, Ytest_ind)
-        LLtest.append(lltest)
-        
-        err = error_rate(p_y_test, Ytest)
-        CRtest.append(err)
-
-        W += lr*(gradW(Ytrain_ind, p_y, Xtrain) - reg*W)
-        b += lr*(gradb(Ytrain_ind, p_y) - reg*b)
-        if i % 10 == 0:
-            print("Cost at iteration %d: %.6f" % (i, ll))
-            print("Error rate:", err)
-
-    p_y = forward(Xtest, W, b)
-    print("Final error rate:", error_rate(p_y, Ytest))
-    iters = range(len(LL))
-    plt.plot(iters, LL, iters, LLtest)
-    plt.show()
-    plt.plot(CRtest)
-    plt.show()
-
-
-def benchmark_pca():
-    X, Y, _, _ = get_transformed_data()
-    X = X[:, :300]
-
-    # normalize X first
-    mu = X.mean(axis=0)
-    std = X.std(axis=0)
-    X = (X - mu) / std
-
-    print("Performing logistic regression...")
-    Xtrain = X[:-1000,]
-    Ytrain = Y[:-1000]
-    Xtest  = X[-1000:,]
-    Ytest  = Y[-1000:]
-
-    N, D = Xtrain.shape
-    Ytrain_ind = np.zeros((N, 10))
-    for i in range(N):
-        Ytrain_ind[i, Ytrain[i]] = 1
-
-    Ntest = len(Ytest)
-    Ytest_ind = np.zeros((Ntest, 10))
-    for i in range(Ntest):
-        Ytest_ind[i, Ytest[i]] = 1
-
-    W = np.random.randn(D, 10) / 28
-    b = np.zeros(10)
-    LL = []
-    LLtest = []
-    CRtest = []
-
-    # D = 300 -> error = 0.07
-    lr = 0.0001
-    reg = 0.01
-    for i in range(200):
-        p_y = forward(Xtrain, W, b)
-        # print "p_y:", p_y
-        ll = cost(p_y, Ytrain_ind)
-        LL.append(ll)
-
-        p_y_test = forward(Xtest, W, b)
-        lltest = cost(p_y_test, Ytest_ind)
-        LLtest.append(lltest)
-
-        err = error_rate(p_y_test, Ytest)
-        CRtest.append(err)
-
-        W += lr*(gradW(Ytrain_ind, p_y, Xtrain) - reg*W)
-        b += lr*(gradb(Ytrain_ind, p_y) - reg*b)
-        if i % 10 == 0:
-            print("Cost at iteration %d: %.6f" % (i, ll))
-            print("Error rate:", err)
-
-    p_y = forward(Xtest, W, b)
-    print("Final error rate:", error_rate(p_y, Ytest))
-    iters = range(len(LL))
-    plt.plot(iters, LL, iters, LLtest)
-    plt.show()
-    plt.plot(CRtest)
-    plt.show()
-
-
-if __name__ == '__main__':
-    benchmark_pca()
-    # benchmark_full()
 
